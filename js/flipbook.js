cfead247de86b0401d6ea638c25a6655208dc081
David Rajchenbach-Teller <dteller@mozilla.com>
2012-12-21 01:49:57 +0100

    [Cleanup] Separated flipbook.js

--- js/flipbook.js
+++ js/flipbook.js
@@ -1,7 +1,124 @@
-// A book that can be flipped through
+(function(obj) {
 
-var FlipBook = function FlipBook(id) {
-};
+  // Dependencies
 
-FlipBook.prototype = {
-};
+  var console = obj.console;
+  var Promise = obj["promise/core"];
+
+
+  var FlipBook = function FlipBook(parent, options) {
+    this._map = new window.Map();
+    this._bufferBefore = options.bufferBackwardsSize;
+    this._bufferAfter = options.bufferForwardsSize;
+    this._book = null;
+    this._page = 0;
+    this._previousPage = -1;
+    this._parent = parent;
+  };
+  FlipBook.prototype = {
+    get book() {
+      return this._book;
+    },
+    set book(book) {
+      this._book = book;
+      this._page = 0;
+    },
+    set page(page) {
+      if (!this._book) {
+        return;
+      }
+      if (this._page == page) {
+        return;
+      }
+      this._previousPage = this._page;
+      this._page = page;
+    },
+    get page() {
+      return this._page;
+    },
+    display: function display() {
+      console.log("Display of page", this.page,
+      "has been requested - I come from page", this._previousPage);
+      var page = this.page;
+      var promise = this.fetch(page);
+      var self = this;
+      var eltPages = this._parent;
+      eltPages.classList.add("loading");
+      return Promise.withLog(promise.then(
+        function onSuccess(data) {
+          var log = window.console.log.bind(window.console, "display onSucess");
+          log("I should now display the data for page", page);
+          if (page != self.page) {
+            log("FIXME", "Do not display", pageNum, "we have moved away from that page");
+          }
+          eltPages.classList.remove("loading");
+          if ("directory" in data) {
+            log("Page is a directory", data.directory);
+            eltPages.textContent = "Directory " + data.directory;
+            return;
+          }
+          if ("imgURL" in data) {
+            log("Page is an image");
+            var date1 = Date.now();
+            eltPages.innerHTML = "";
+            var img = document.createElement("img");
+            eltPages.appendChild(img);
+            img.src = data.imgURL;
+            return;
+          }
+          console.error("Unrecognized data", data);
+        }));
+    },
+    fetch: function fetch(pageNum) {
+      // Don't bother fetching the page if it is negative
+      if (pageNum < 0) {
+        return Promise.reject(obj.Book.Error.NO_MORE_PAGES);
+      }
+
+      // Starting page prefetch
+      var i;
+      var pages = [];
+      for (i = pageNum; i <= pageNum + this._bufferAfter; ++i) {
+        pages.push(i);
+      }
+      for (i = Math.max(0, pageNum - 1); i >= pageNum - this._bufferBefore; --i) {
+        pages.push(i);
+      }
+      var self = this;
+      pages.forEach(function(page) {
+        var promise = self._map.get(page);
+        if (!promise) {
+          console.log("Pre-fetching page", page);
+          promise = self.book.displayPage(page);
+          self._map.set(page, promise);
+          promise.then(
+            function onSuccess(e) {
+              console.log("Pre-fetching complete", page);
+              return e;
+            },
+            function onError(e) {
+              console.log("Pre-fetching failed", page, e);
+              throw e;
+            }
+          );
+        } else {
+          console.log("Page alread pre-fetched", page);
+        }
+      });
+      // Cleaning up cache
+      for (var [k, val] of this._map) {
+        if (k < pageNum - self._bufferBefore || k > pageNum + self._bufferAfter) {
+          console.log("Removing from cache page", pageNum);
+          if ("dispose" in val) {
+            val.dispose();
+          }
+          self._map.delete(k);
+        }
+      };
+      // Finally return the intended page
+      return this._map.get(pageNum);
+    }
+  };
+
+  obj.FlipBook = FlipBook;
+})(window);
--- js/main.js
+++ js/main.js
@@ -2,16 +2,14 @@
   "use strict";
 
   // Options
-  var ltr = true;
-  var buffer_forwards_size = 3;
-  var buffer_backwards_size = 1;
+  var Options = {
+    ltr: true,
+    bufferForwardsSize: 3,
+    bufferBackwardsSize: 1
+  };
 
   // Setup zip
   obj.zip.workerScriptsPath = "lib/zip.js/WebContent/";
-
-  // The book
-  var book = null;
-  var currentPage = null;
 
   // Get rid of warnings
   var console = window.console;
@@ -25,95 +23,26 @@
   || function emulatedRequestAnimationFrame(step) {
     window.setTimeout(step, 25);
   };
-
-  var Pages = {
-    _map: new Map(),
-    _bufferBefore: 1,
-    _bufferAfter: 3,
-    display: function display(pageNum) {
-      var promise = this.fetch(pageNum);
-      eltPages.classList.add("loading");
-      return promise.then(
-        function onSuccess(data) {
-          if (pageNum != currentPage) {
-            console.log("FIXME", "Do not display", pageNum, "we have moved away from that page");
-          }
-          eltPages.classList.remove("loading");
-          if ("directory" in data) {
-            eltPages.textContent = "Directory " + data.directory;
-            return;
-          }
-          if ("imgURL" in data) {
-            var date1 = Date.now();
-            eltPages.innerHTML = "";
-            var img = document.createElement("img");
-            eltPages.appendChild(img);
-            img.src = data.imgURL;
-          }
-        },
-        function onError(e) {
-          console.log("FIXME", "I should handle error", e, "for page", pageNum);
-        });
-
-    },
-    fetch: function fetch(pageNum) {
-      // Don't bother fetching the page if it is negative
-      if (pageNum < 0) {
-        return Promise.reject(Book.Error.NO_MORE_PAGES);
+  var Promise = window["promise/core"];
+  Promise.withLog = function(promise) {
+    return promise.then(null,
+      function onError(e) {
+        console.error("Uncaught promise error", e);
+        throw e;
       }
-
-      // Starting page prefetch
-      var i;
-      var pages = [];
-      for (i = pageNum; i <= pageNum + this._bufferAfter; ++i) {
-        pages.push(i);
-      }
-      for (i = Math.max(0, pageNum - 1); i >= pageNum - this._bufferBefore; --i) {
-        pages.push(i);
-      }
-      var self = this;
-      pages.forEach(function(page) {
-        var promise = self._map.get(page);
-        if (!promise) {
-          console.log("Pre-fetching page", page);
-          promise = book.displayPage(page);
-          self._map.set(page, promise);
-          promise.then(
-            function onSuccess(e) {
-              console.log("Pre-fetching complete", page);
-              return e;
-            },
-            function onError(e) {
-              console.log("Pre-fetching failed", page, e);
-              throw e;
-            }
-          );
-        } else {
-          console.log("Page alread pre-fetched", page);
-        }
-      });
-      // Cleaning up cache
-      for (var [k, val] of this._map) {
-        if (k < pageNum - self._bufferBefore || k > pageNum + self._bufferAfter) {
-          console.log("Removing from cache page", pageNum);
-          if ("dispose" in val) {
-            val.dispose();
-          }
-          self._map.delete(k);
-        }
-      };
-      // Finally return the intended page
-      return this._map.get(pageNum);
-    }
+    );
   };
 
-
-  // File picker
+  // Elements
   var eltFilePickerControl = document.getElementById("pick_file_control");
   var eltFilePickerTab = document.getElementById("pick_file_tab");
   var eltPages = document.getElementById("pages");
   var eltWelcome = document.getElementById("welcome");
 
+
+  var flipBook = new obj.FlipBook(eltPages, Options);
+
+  // User Interface
   eltFilePickerTab.addEventListener("click", function oncommand(e) {
     eltFilePickerControl.click();
   });
@@ -131,36 +60,37 @@
     for (i = 0; i < eltFilePickerControl.files.length; ++i) {
       files.push(eltFilePickerControl.files[i]);
     }
-    book = new obj.Book(files);
-    Pages.display(currentPage = 0);
+    flipBook.book = new obj.Book(files);
+    flipBook.page = 0;
+    flipBook.display();
     eltWelcome.style.display = "none";
   });
 
   var onkey = function onkey(event) {
-    if (!book) {
-      return;
-    }
     var code;
+    var delta;
     if ("keyCode" in event || "which" in event) {
       code = event.keyCode || event.which;
       if ( // FIXME: Chrome doesn't define KeyEvent
+           // FIXME: LEFT and RIGHT should depend on Options.ltr
         code == KeyEvent.DOM_VK_DOWN
           || code == KeyEvent.DOM_VK_PAGE_DOWN
           || code == KeyEvent.DOM_VK_RIGHT
           || code == KeyEvent.DOM_VK_SPACE
          ) {
-        ++currentPage;
+           delta = 1;
       } else if (
         code == KeyEvent.DOM_VK_UP
           || code == KeyEvent.DOM_VK_PAGE_UP
           || code == KeyEvent.DOM_VK_LEFT
           || code == KeyEvent.DOM_VK_BACK_SPACE
       ) {
-        --currentPage;
+        delta = -1;
       } else {
         return;
       }
-      Pages.display(currentPage);
+      flipBook.page += delta;
+      flipBook.display();
     }
   };
 
@@ -170,6 +100,6 @@
   // Debugging code
 
   window.OpenBerg = {
-    Pages: Pages
+    flipBook: flipBook
   };
 })(this);
